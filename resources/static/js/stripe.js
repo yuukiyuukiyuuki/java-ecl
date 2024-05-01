1f88717fe1e07b98d0e110519aa9d5cda7833917
yuukiyuukiyuuki <128203554+yuukiyuukiyuuki@users.noreply.github.com>
2024-05-01 21:04:23 +0900

    追加する

---
+++ resources/static/js/stripe.js
@@ -0,0 +1,8 @@
+const stripe = Stripe('pk_test_51PACGEEidPDeDeb4a3ociLzpk_test_51PACGEEidPDeDeb4a3ociLz40wnHXz680owe6AsW215y2S1uKSnrkF5YjuMAfYgDpELMIAZXgr9q6Buxch1beJnA00xc6Hvqs740wnHXz680owe6AsW215y2S1uKSnrkF5YjuMAfYgDpELMIAZXgr9q6Buxch1beJnA00xc6Hvqs7');
+ const paymentButton = document.querySelector('#paymentButton');
+ 
+ paymentButton.addEventListener('click', () => {
+   stripe.redirectToCheckout({
+     sessionId: sessionId
+   })
+ });
